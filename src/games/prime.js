61184a80df8c8579224947748b2eef4122095596
ysromantic <ysromantic@gmail.com>
2024-08-10 23:24:25 +0300

    add prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+import { game, getRandom } from '../index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+    if (num < 2) return false;
+    for (let divider = 2; divider <= Math.sqrt(num); divider += 1) {
+        if (num % divider === 0) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const getQuestionAndAnswer = () => {
+    const question = getRandom(1, 100);
+    const correctAnswer = isPrime(question) ? 'yes' : 'no';
+    return [question, correctAnswer];
+};
+
+export const primeGame = () => {
+    game(description, getQuestionAndAnswer);
+};
